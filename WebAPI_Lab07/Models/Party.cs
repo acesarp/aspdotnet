14643ad92ac362bf3cf0d1fb87d59ac2f0c608c3
acesarp <acesarp@outlook.com>
2018-10-27 12:00:26 -0700

    1st commit

---
+++ WebAPI_Lab07/Models/Party.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WebAPI_Lab07.Models {
+
+    public class Party {
+
+        public Party(int id, string name, DateTime date, int numOfGuests, string location){ 
+            PartyId = id;
+            PartyName = name;
+            PartyDate = date;
+            NumberOfGuests = numOfGuests;
+            Location = location;
+
+        }
+
+        public int PartyId { get; set; }
+        string PartyName { get; set; }
+        public DateTime PartyDate { get; set; }
+        public int NumberOfGuests {get; set;}
+        public string Location {get; set;}
+    }
+}
